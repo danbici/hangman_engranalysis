a71efff8ec408cbf21434117229fc2668c34adce
danbici <64282927+danbici@users.noreply.github.com>
2020-04-26 19:16:00 -0400

    Add files via upload

--- hangman_matlab.m
+++ hangman_matlab.m
@@ -8,46 +8,187 @@
 % Brian   O'Connell
 
 
-function board_gui
-% This function takes no arguments and creates a hangman gui
-% That will interact with various functions
 
-   %  Create a figure for the board and turn it off to load the UI
-   f = figure('Visible','off','Position',[400,500,450,285]);
-   
-   %  Push button to Start a New game. This will end the current game
-   %  But will NOT solve the game for you.
-   %
-   hnew_game = uicontrol('Style','pushbutton','String','New Game',...
-           'Position',[315,220,70,25]);
-   
-   % Push button to Solve game. This will produce the word for you
-   %
-   hsolve = uicontrol('Style','pushbutton','String','Solve Game',...
-           'Position',[315,180,70,25]);
-   
-   % Text  to enter the letter to play the game    
-   %
-   htext_box = uicontrol('Style','text','String','Enter a letter',...
-           'Position',[315,100,70,15]);
-   
-   % Text box to enter the letter
-   %
-   hletter = uicontrol('Style','edit','Position',[315, 80,70, 15]);
+function Hangman
+% This is the main function to run the program
 
-   % This will get the user input letter and pass it
-   %
-   guessed_letter = get(hletter,'String');
+fprintf("Welcome to hangman\n");
+fprintf("\n");
 
-   
-   %Create the hangman guillotine
-   %
-   % CURRENTLY PRODUCES A GRAPH
-   %ha = axes('Units','Pixels','Position',[50,60,200,185]); 
-   %align([hnew_game,hsolve,htext_box,hletter],'Right','None');
-   
-   
-   % Make the UI visible
-   f.Visible = 'on';
+% Define some variables
+%
+Used_letters = [];
+
+% Randomly generate a word to pick based upon a test case
+%
+generate_case = randi(5);
+
+% chooses a word to play with
+%
+switch generate_case
+    case 1
+        word = 'matrices';
+    case 2
+        word = 'eigenvector';
+    case 3
+        word = 'identity';
+    case 4
+        word = 'transpose';
+    case 5
+        word = 'determinant';
+end
+
+% find the length of the word
+%
+word_length = strlength(word)
+
+for i=1:word_length
+    if(isletter)
+        word[i] = 
+
+
+
+
+% Call the noose function to draw it
+%
+DrawNoose()
+
+% Go into a loop as long as you can guess a letter
+%
+%while 1
+    
+
+%    word_a = 
+    
+% call the word checker function
+%
+%word_checker(word, vargin(1))
+%
+%end
+
+
+end
+
+
+function word_checker(word_a, letter_a)
+% This function checks if the letter provided matches the word
+% That has been randomly generated
+% letter_a : the letter provided by the user
+% word_a: the word generated
+
+% check if the letter is in the word
+%
+
+% Ask the user to input a letter:
+%
+fprintf("Input a letter:\n")
+
+if isempty(strfind(word_a, letter_a))
+    Used_words = append(used_words, letter_a);
+  
+    % CALL THE HANGMAN BODYPART FUNCTION HERE
+    % !!!!
+    AmountWrong = AmountWrong + 1;
+    
+    % call the hangman bodypart function
+    %
+    DrawHangingMan(AmountWrong)
+    
+    
+    if AmountWrong == 6
+        quit;
+    end
+    
+else
+    % CALL THE FUNCTION THAT DISPLAYS YOU GOT THE LETTER!
+    %!!!!
+    %guessed
+    
+end
+
+end
+
+
+% call the noose function to draw the noose
+%
+function DrawNoose()
+
+rectangle('Position', [5 5 5 5],'linewidth',5,'edgecolor','k');
+
+S1= line([9,9],[9,5]);
+S1.Color = 'k'; 
+S1.LineWidth = 4;
+
+S2= line([7.5,9],[9,9]);
+S2.Color = 'k'; 
+S2.LineWidth = 4;
+
+S2= line([7.5,7.5],[8,9]);
+S2.Color = 'k'; 
+S2.LineWidth = 4;
+
+end
+
+
+
+function DrawHangingMan(AmountWrong)
+
+hold on
+
+if (AmountWrong == 1)
+hold on
+%Draws head
+x=7.5;
+y=7.75;
+r=0.25;
+theta = 0:pi/100:2*pi;
+a= r*cos(theta)+x;
+b=r*sin(theta)+y;
+Head = plot(a,b);
+Head.LineWidth = 4;
+Head.Color = 'b';
+end
+
+if (AmountWrong == 2)
+
+%Draws Body
+hold on
+Body= line([7.5,7.5],[7.5,6]);
+Body.Color = 'b'; 
+Body.LineWidth = 4;
+end
+
+if (AmountWrong == 3)
+%Draws Left Leg
+hold on
+LeftLeg = line([7.5,7],[6,5.5]);
+LeftLeg.Color = 'b'; 
+LeftLeg.LineWidth = 4;
+end
+
+if (AmountWrong == 4)
+%Draws Right Leg
+hold on
+LeftLeg = line([7.5,8],[6,5.5]);
+LeftLeg.Color = 'b'; 
+LeftLeg.LineWidth = 4;
+end
+
+if (AmountWrong == 5)
+%Draws Right Arm
+hold on
+RightArm = line([7.5,7],[7,7.5]);
+RightArm.Color = 'b'; 
+RightArm.LineWidth = 4;
+end
+
+if (AmountWrong == 6)
+%Draws Left Arm
+hold on
+LeftArm = line([7.5,8],[7,7.5]);
+LeftArm.Color = 'b'; 
+LeftArm.LineWidth = 4;
+
+end
 
 end
